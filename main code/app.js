87dfb155c95dfa3cb25f729cef18fc934faa3ee8
Rebecca Kempe <RebeccaK@hyivy.com>
2021-02-07 09:30:19 -0500

    Trying to fix the problem

--- main code/app.js
+++ main code/app.js
@@ -1,47 +1,3 @@
-<<<<<<< HEAD
-var canvas = document.createElement("canvas");
-var ctx = canvas.getContext("2d");
-canvas.width = 1000;
-canvas.height = 500;
-document.body.appendChild(canvas);
-canvas.style.backgroundImage=("url('sand.jpg')");
-
-var tile = {
-    width: canvas.width/40,
-    height: canvas.height/20,
-    x: 0,
-    y: 0
-};
-//hey
-
-// const tileArray = [];  // bottom right corner of each tile
-
-// for (i = 0; i < 40; i++) {
-//     tile.x += tile.width;
-
-//     for (j = 0; i < 20; j++) {
-//         tile.y += tile.height;
-
-//         tileArray.push([tile.x,tile.y]);
-
-//     }
-//}
-
-//console.log(tileArray);
-
-
-var hero = {
-    x: 50,
-    y: 50
-};
-
-function updateHero() {
-ctx.clearRect(0, 0, canvas.width, canvas.height);
-ctx.beginPath();
-ctx.rect(hero.x,hero.y,50,50);
-ctx.stroke();
-ctx.endPath();
-=======
 var container = document.getElementById("container")
     var canvas = document.createElement("canvas");
     var ctx = canvas.getContext("2d");
@@ -73,7 +29,6 @@ function updateCharacter() {
     ctx.stroke();
     ctx.endPath();
     ctx.fillText(columns,10,10);
->>>>>>> 143a2b06c3facc29c6c1b7ab7687772eb5f5f728
 }
 
 
