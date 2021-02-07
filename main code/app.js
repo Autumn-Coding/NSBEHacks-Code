667426b3d9d7510683f17e612177513f91bc5abc
Kate <dgmk9-k@yahoo.com>
2021-02-07 12:02:24 +0900

    t

--- main code/app.js
+++ main code/app.js
@@ -1,36 +1,41 @@
 var canvas = document.createElement("canvas");
 var ctx = canvas.getContext("2d");
-canvas.width = 512;
-canvas.height = 480;
+canvas.width = 500;
+canvas.height = 500;
 document.body.appendChild(canvas);
 
 
 canvas.style.backgroundColor = 'grey';
 var hero = {
-    x: 0,
-    y: 0
+    x: 50,
+    y: 50
 };
 
 function updateHero() {
+ctx.clearRect(0, 0, canvas.width, canvas.height);
 ctx.beginPath();
-ctx.rect(hero.x,hero.y,10,10);
+ctx.rect(hero.x,hero.y,50,50);
 ctx.stroke();
 ctx.endPath();
 }
 
-updateHero()
 
 
-canvas.onkeypress = function(e) {
+
+document.addEventListener('keydown', keyPressed);
+
+function keyPressed(e) {
     var key = e.which;
     if (key == 37) {
-        hero.x += -10
+        hero.x += -50;
     }  else if (key == 39) {
-        hero.x += 10
+        hero.x += 50;
     }   else if (key == 38) {
-        hero.y += 10
+        hero.y += -50;
     }   else if (key == 40) {
-        hero.y += -10
+        hero.y += 50;
     }
     updateHero()
 }
+
+document.onload = updateHero();
