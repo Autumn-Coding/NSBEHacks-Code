e98d05503e58fd5295fc702bae6fa9c8522bb1c6
Kate <dgmk9-k@yahoo.com>
2021-02-07 23:36:57 +0900

    t

--- main code/app.js
+++ main code/app.js
@@ -4,35 +4,45 @@ var container = document.getElementById("container")
     canvas.width = 1000;
     canvas.height = 500;
     canvas.style.backgroundImage = 'url("sand.jpg")';
+    canvas.style.border = "2pt solid black"
     container.appendChild(canvas);
 
-    var character = {
-        x: 0,
-        y: 0
-    };
-
-    var tileSide = 20;
+    var tileSide = 50;
     var gameColumns = [];
     var gameRows = [];
 
 
-    for(x = 0; x <= canvas.width - tileSide; x += tileSide) {
+    for(x = tileSide; x <= canvas.width - tileSide; x += tileSide) {
         gameColumns.push(x)
     }
-    for(y = 0; y <= canvas.height - tileSide; y += tileSide) {
+    for(y = tileSide; y <= canvas.height - tileSide; y += tileSide) {
         gameRows.push(y)
     }
 
+    var character = {
+        x: gameColumns[0],
+        y: gameRows[0]
+    };
+
+    var goal = {
+        x: gameColumns[15],
+        y: gameRows[7]
+    }
 
 function updateCharacter() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.beginPath();
+    ctx.rect(goal.x,goal.y,tileSide,tileSide);
     ctx.rect(character.x,character.y,tileSide,tileSide);
     ctx.stroke();
     ctx.endPath();
 }
 
-
+function checkSpace(axis, direction) {
+    if (character.axis = x) {
+        if
+    }
+}
 
 
 document.addEventListener('keydown', keyPressed);
@@ -40,9 +50,15 @@ document.addEventListener('keydown', keyPressed);
 function keyPressed(e) {
     var key = e.which;
     if (key == 37 && character.x > gameColumns[0]) {
-        character.x += -tileSide;
+        var checked = checkSpace(x, left);
+        if (checked == true) {
+            character.x += -tileSide;
+        }
     }  else if (key == 39 && character.x + tileSide < canvas.width) {
-        character.x += tileSide;
+        var checked = checkSpace();
+        if (checked == true) {
+            character.x += tileSide;
+        }
     }   else if (key == 38 && character.y > gameRows[0]) {
         character.y += -tileSide;
     }   else if (key == 40 && character.y + tileSide < canvas.height) {
